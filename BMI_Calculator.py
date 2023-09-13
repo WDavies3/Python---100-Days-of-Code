0a9ed125ed4f2f469ee91be0a04d8aa95bd805f7
WDavies3 <38267653+WDavies3@users.noreply.github.com>
2023-09-13 10:41:20 -0400

    BMI Calculator

---
+++ BMI_Calculator.py
@@ -0,0 +1,10 @@
+#Enter your Height in inches
+height = int(input("Enter your height in inches? "))
+#Enter your Weight in pounds
+weight = int(input("Enter your weight in pounds? "))
+#calculate your BMI
+BMI = int(weight / (height**2) * 703)
+#print the BMI
+print(f"Your BMI is {BMI}")
+
+
