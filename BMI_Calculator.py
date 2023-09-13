c86d71c230c91884f5c8bf48713e5a548d6365db
WDavies3 <38267653+WDavies3@users.noreply.github.com>
2023-09-13 10:51:58 -0400

    update rounding

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -3,7 +3,7 @@ height = int(input("Enter your height in inches? "))
 #Enter your Weight in pounds
 weight = int(input("Enter your weight in pounds? "))
 #calculate your BMI
-BMI = int(weight / (height**2) * 703)
+BMI = round(weight / (height**2) * 703)
 #print the BMI
 print(f"Your BMI is {BMI}")
 
