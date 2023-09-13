dbf9c1e1e04ca9f6360bfe26c36cbc945d1b7ef0
WDavies3 <38267653+WDavies3@users.noreply.github.com>
2023-09-13 11:29:44 -0400

    limit number of decimal places in result

--- TipCalculator.py
+++ TipCalculator.py
@@ -8,7 +8,7 @@ num_of_ppl_paying = float(input("How many people are splitting the bill? "))
 percent_tip = float(input("How much do you want to tip? (15, 20, or 22 percent) "))
 
 #Calculate the tip
-cost_per_person = total/num_of_ppl_paying*(1 + percent_tip/100)
+cost_per_person = round(total/num_of_ppl_paying*(1 + percent_tip/100), 2)
 
 #print the result
 print("Your cost is " + str(cost_per_person))
