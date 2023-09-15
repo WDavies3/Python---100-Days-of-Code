ffe911566b01487e6510b9509d16a0308cbbb22a
WDavies3 <38267653+WDavies3@users.noreply.github.com>
2023-09-15 16:25:22 -0400

    random result 2 methods

--- RandomName.py
+++ RandomName.py
@@ -3,6 +3,10 @@ import random
 def getName(names=[]):
     return names[random.randint(0, len(names)-1)]
 
+def getName2(names=[]):
+    return random.choice(names)
+
 strNames = input("Enter a list of names seperated by commas: ")
 lstNames = strNames.split(", ")
 print(f"The randomly selected name is {getName(lstNames)}")
+print(f"The second name is {getName2(lstNames)}")
