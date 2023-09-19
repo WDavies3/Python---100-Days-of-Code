899977ab064afdba25ee13a2625b26598290943d
WDavies3 <38267653+WDavies3@users.noreply.github.com>
2023-09-18 23:10:32 -0400

    update hangman

--- Hangman.py
+++ Hangman.py
@@ -1,77 +1,5 @@
-import random, os
-
-hangman_image = {
-    0:'''
-      _______
-     |/      
-     |      
-     |      
-     |       
-     |      
-     |
-    _|___
-''',
-    
-    1:'''
-      _______
-     |/      |
-     |      (_)
-     |      
-     |       
-     |      
-     |
-    _|___
-''',
-    2:'''
-      _______
-     |/      |
-     |      (_)
-     |       |
-     |       |
-     |      
-     |
-    _|___
-''',
-    3:'''
-      _______
-     |/      |
-     |      (_)
-     |      \|
-     |       |
-     |      
-     |
-    _|___
-''',
-    4:'''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      
-     |
-    _|___
-''',
-    5:'''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / 
-     |
-    _|___
-''',
-    6:'''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / \\
-     |
-    _|___'''
-}
+import random, os, Hangman_Art
+from Hangman_Words import word_list
 
 def print_word(letters, word):
     display = ""
@@ -90,17 +18,15 @@ def print_remaining_letters(letters):
         display += c
         display += ' '
     print(display)
-    
-words = ["underwear", "diaper", "glasses", "elephant"]
-heading = "Hangman"
+
 mistakes = 0
 letters = []
 remaining_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-word = random.choice(words)
+word = random.choice(word_list)
 while mistakes < 6:
     os.system('cls')
-    print(heading)
-    print(hangman_image[mistakes])
+    print(Hangman_Art.hangman_logo)
+    print(Hangman_Art.hangman_image[mistakes])
     if print_word(letters, word):
         break
     print("Letters Remaining:")
@@ -118,8 +44,8 @@ while mistakes < 6:
     
     
 os.system('cls')
-print(heading)
-print(hangman_image[mistakes])
+print(Hangman_Art.hangman_logo)
+print(Hangman_Art.hangman_image[mistakes])
 print_word(letters, word)
 if mistakes < 6:
     print("You Win!!!")
